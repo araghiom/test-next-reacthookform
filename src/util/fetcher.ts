c868e8240ac7726b1ed6a96a8ae12f67c7974002
araghiom <araghiom@gmail.com>
2025-11-24 04:13:09 +0330

    feat: add utility functions and hooks for data fetching and theme management
    - Implemented a fetcher utility for API calls with error handling.
    - Added a custom hook for debouncing values.
    - Created a theme management hook to handle light and dark modes with local storage support.
    - Introduced a placeholder for SEO schema management.

---
+++ src/util/fetcher.ts
@@ -0,0 +1,35 @@
+import { SearchParamsObject } from "@/types/common.type"
+
+
+const fetcher = async <K>(
+  endpoint: string,
+  options?: SearchParamsObject
+): Promise<K> => {
+  const searchParams = new URLSearchParams(
+    options?.searchParams as Record<string, string>
+  )
+  const res = await fetch(
+    `${endpoint}${
+      searchParams.toString() ? `?${searchParams.toString()}` : ""
+    }`,
+    { ...options, credentials: "include" }
+  )
+
+  const resJson = await res.json()
+
+  if (!res.ok) {
+    if (resJson?.code === "rest_post_invalid_page_number") {
+      return {
+        data: [],
+        totalResults: 0,
+      } as any
+    }
+    // throw new Error(resJson.message);
+  }
+
+
+
+  return resJson
+}
+
+export default fetcher
